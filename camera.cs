936bba60763d9748d353233c9b885fe6a75378d8
weezfort <83803998+weezfort@users.noreply.github.com>
2021-10-14 13:40:20 +0900

    Add files via upload

---
+++ camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public GameObject target;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
