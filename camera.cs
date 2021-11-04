612f90e2f8d7b6a0ee33f9418c3a49b6285be381
weezfort <83803998+weezfort@users.noreply.github.com>
2021-11-04 13:44:49 +0900

    Add files via upload

--- camera.cs
+++ camera.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class camera : MonoBehaviour
 {
-    public Transform target;
-    private Vector3 offset;
+    public Transform target; // ターゲットへの参照
+    private Vector3 offset; // 相対座標
     void Start()
     {
+        //自分自身と target との相対距離を求める
         offset = GetComponent<Transform>().position - target.position;
     }
     void Update()
     {
-    GetComponent<Transform>().position = target.position + offset;
+        // 自分自身の座標に、target の座標に相対座標を足した値を設定する
+    GetComponent<Transform>().position =
+target.position + offset;
     }
 }
